8d95e69d4fd964ea2743c0229adb55c4158936a5
Yurarara <naibrgood@gmail.com>
2025-06-22 15:12:50 +0300

    [chore] remove comments and useless imports

--- webest-test/src/store/store.js
+++ webest-test/src/store/store.js
@@ -1,17 +1,15 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 
 export const useCatalogStore = defineStore("catalog", () => {
   const activeFilters = ref([]);
 
-  // Добавление фильтра
   const addFilter = (filter) => {
     if (!activeFilters.value.includes(filter)) {
       activeFilters.value.push(filter);
     }
   };
 
-  // Удаление фильтра
   const removeFilter = (filter) => {
     activeFilters.value = activeFilters.value.filter((f) => f !== filter);
   };
